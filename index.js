8739200d0b97a4477819015e107190092478de58
rémi  <remirobert33530@gmail.com>
2016-03-18 23:13:44 +0800

    initiale release

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var request = require('request');
+var promise = require('promise');
+
+/**
+* Initialisation of the module.
+* @param {clientId} Foorsquare clientId.
+* @return {clientSecret} Foorsquare clientSecret.
+*/
+module.exports = function(clientId, clientSecret) {
+  var requestUrl = "https://api.foursquare.com/v2/venues/explore"
+
+  if (clientId && clientSecret) {
+    requestUrl += "?client_id=" + clientId + "&client_secret=" + clientSecret;
+  }
+  else {
+    throw "You need to specify your Foorsquare client_id and client_secret."
+  }
+
+  function optionsParameters(options, name, defaultValue) {
+    return options && options[name] !== undefined ? options[name] : defaultValue;
+  }
+
+  var module = {
+    /**
+    * find venue by specified options.
+    * @param {options} dictionary containing options for searching the venue.
+    * @return {promise} return a promise block to handle the error and the response.
+    */
+    find: function(options) {
+      requestUrl += "&v=20130815&ll=" + optionsParameters(options, 'lat', 40.7) + "," + optionsParameters(options, 'lng', -74);
+      requestUrl += "&limit=" + optionsParameters(options, 'limit', 10);
+      requestUrl += "&venuePhotos=" + optionsParameters(options, 'venuePhotos', 1);
+      requestUrl += "&section=" + optionsParameters(options, 'section', "food");
+
+      console.log(options.lat);
+      console.log(requestUrl);
+      return new Promise(function (resolve, reject) {
+        request(requestUrl, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var json = JSON.parse(body);
+            var venues = json.response.groups[0].items.map(function(item) {
+              var venue = item.venue
+              var photos = venue.photos.groups[0].items.map(function(photo) {
+                return photo.prefix + "612x612" + photo.suffix;
+              });
+              return {
+                name: venue.name,
+                location: {
+                  address: venue.location.formattedAddress,
+                  lat: venue.location.lat,
+                  lng: venue.location.lng,
+                  distabce: venue.location.distance
+                },
+                rating: venue.rating,
+                photos: photos
+              };
+            });
+            resolve(venues);
+          }
+          else if (error) {
+            resolve(error);
+          }
+          else {
+            resolve("Invalid request");
+          }
+        });
+      });
+    }
+  };
+  return module;
+};
---
+++ test/test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var clientId = "AREWTY4KWJEDLALROY0VZWTJ5XFTCITLH2LXZU3WP2OJ1MQW";
+var clientSecret = "M2JLJHXBY2CO2V1F05XZMCH3U143BO5MTFDK3MRV0WB5DSG0";
+
+var expect = require('chai').expect;
+var venueGenerator = require('../index')(clientId, clientSecret);
+
+describe('#venueGenerator', function() {
+  it('should get 5 food venues', function(done) {
+    venueGenerator.find({limit: 5})
+    .then(function(venues) {
+      console.log(venues);
+      expect(venues).to.have.length(5);
+      done();
+    })
+    .catch(function(err) {
+      assert.fail(error);
+      done();
+    });
+  });
+});
